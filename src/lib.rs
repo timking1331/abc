0af6286206870cd6f1ed1bc9f850bc48f362e79b
tim kavin <timking1331@outlook.com>
2024-10-31 16:55:28 +0530

    Add Default directly & remove new() fn

--- src/lib.rs
+++ src/lib.rs
@@ -7,18 +7,7 @@ pub use todos::*;
 
 pub const API_BASE_URL: &str = "https://dummyjson.com";
 
+#[derive(Default)]
 pub struct DummyJsonClient {
 	pub client: Client,
-}
-
-impl DummyJsonClient {
-	pub fn new() -> Self {
-		Self { client: Client::new() }
-	}
-}
-
-impl Default for DummyJsonClient {
-	fn default() -> Self {
-		Self::new()
-	}
 }
