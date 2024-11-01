86b0a78efa99d6ae9728a31d0cbc578322ee51e9
tim kavin <timking1331@outlook.com>
2024-11-02 00:05:51 +0530

    Complete posts module along with tests

--- src/posts.rs
+++ src/posts.rs
@@ -1,22 +1,28 @@
-use crate::{DummyJsonClient, API_BASE_URL};
+use crate::{DummyJsonClient, ProductCategory, API_BASE_URL};
 use once_cell::sync::Lazy;
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
 static POSTS_BASE_URL: Lazy<String> = Lazy::new(|| format!("{}/posts", API_BASE_URL));
 
-#[derive(Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Default)]
 pub struct Post {
 	pub id: u32,
-	pub title: String,
-	pub body: String,
-	pub tags: Vec<String>,
-	pub reactions: Reaction,
-	pub views: u32,
-	#[serde(rename = "userId")]
-	pub user_id: u32,
+	#[serde(flatten)]
+	pub other_fields: AddPost,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Default)]
+pub struct AddPost {
+	pub title: Option<String>,
+	pub body: Option<String>,
+	pub tags: Option<Vec<String>>,
+	pub reactions: Option<Reaction>,
+	pub views: Option<u32>,
+	#[serde(rename = "userId")]
+	pub user_id: Option<u32>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Reaction {
 	pub likes: u32,
 	pub dislikes: u32,
@@ -25,6 +31,32 @@ pub struct Reaction {
 #[derive(Deserialize, Debug)]
 pub struct GetAllPosts {
 	pub posts: Vec<Post>,
+	pub total: u32,
+	pub skip: u32,
+	pub limit: u32,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct PostComment {
+	pub id: u32,
+	pub body: String,
+	#[serde(rename = "postId")]
+	pub post_id: u32,
+	pub likes: u32,
+	pub user: UserProfile,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct UserProfile {
+	pub id: u32,
+	pub username: String,
+	#[serde(rename = "fullName")]
+	pub full_name: String,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct PostCommentsResponse {
+	pub comments: Vec<PostComment>,
 	pub total: u32,
 	pub skip: u32,
 	pub limit: u32,
@@ -58,6 +90,114 @@ impl DummyJsonClient {
 			.send()
 			.await?
 			.json::<GetAllPosts>()
+			.await
+	}
+
+	/// Limit and skip posts
+	pub async fn limit_and_skip_posts(
+		&self,
+		limit: u32,
+		skip: u32,
+		selects: &str,
+	) -> Result<GetAllPosts, reqwest::Error> {
+		self.client
+			.get(format!(
+				"{}/?limit={}&skip={}&select={}",
+				POSTS_BASE_URL.as_str(),
+				limit,
+				skip,
+				selects
+			))
+			.send()
+			.await?
+			.json::<GetAllPosts>()
+			.await
+	}
+
+	/// Sort posts
+	pub async fn sort_posts(
+		&self,
+		sort_by: &str,
+		// TODO: Change to enum
+		order: &str,
+	) -> Result<GetAllPosts, reqwest::Error> {
+		self.client
+			.get(format!("{}/?sortBy={}&order={}", POSTS_BASE_URL.as_str(), sort_by, order))
+			.send()
+			.await?
+			.json::<GetAllPosts>()
+			.await
+	}
+
+	/// Get all posts tags
+	pub async fn get_all_posts_tags(&self) -> Result<Vec<ProductCategory>, reqwest::Error> {
+		self.client
+			.get(format!("{}/tags", POSTS_BASE_URL.as_str()))
+			.send()
+			.await?
+			.json::<Vec<ProductCategory>>()
+			.await
+	}
+
+	/// Get posts by tags
+	pub async fn get_posts_by_tags(&self, tag: &str) -> Result<GetAllPosts, reqwest::Error> {
+		self.client
+			.get(format!("{}/tag/{}", POSTS_BASE_URL.as_str(), tag))
+			.send()
+			.await?
+			.json::<GetAllPosts>()
+			.await
+	}
+
+	/// Get posts by user id
+	pub async fn get_posts_by_user_id(&self, user_id: u32) -> Result<GetAllPosts, reqwest::Error> {
+		self.client
+			.get(format!("{}/user/{}", POSTS_BASE_URL.as_str(), user_id))
+			.send()
+			.await?
+			.json::<GetAllPosts>()
+			.await
+	}
+
+	/// Get post comments
+	pub async fn get_post_comments(&self, id: u32) -> Result<PostCommentsResponse, reqwest::Error> {
+		self.client
+			.get(format!("{}/{}/comments", POSTS_BASE_URL.as_str(), id))
+			.send()
+			.await?
+			.json::<PostCommentsResponse>()
+			.await
+	}
+
+	/// Add post
+	pub async fn add_post(&self, post: &AddPost) -> Result<Post, reqwest::Error> {
+		self.client
+			.post(format!("{}/add", POSTS_BASE_URL.as_str()))
+			.json(post)
+			.send()
+			.await?
+			.json::<Post>()
+			.await
+	}
+
+	/// Update post
+	pub async fn update_post(&self, id: u32, post: &AddPost) -> Result<AddPost, reqwest::Error> {
+		self.client
+			.put(format!("{}/{}", POSTS_BASE_URL.as_str(), id))
+			.json(post)
+			.send()
+			.await?
+			.json::<AddPost>()
+			.await
+	}
+
+	/// Delete post
+	pub async fn delete_post(&self, id: u32) -> Result<Post, reqwest::Error> {
+		self.client
+			.delete(format!("{}/{}", POSTS_BASE_URL.as_str(), id))
+			.send()
+			.await?
+			.json::<Post>()
 			.await
 	}
 }
--- tests/posts.rs
+++ tests/posts.rs
@@ -1,6 +1,6 @@
 #[cfg(test)]
 mod posts {
-	use dummy_json_rs::DummyJsonClient;
+	use dummy_json_rs::{AddPost, DummyJsonClient};
 
 	#[tokio::test]
 	async fn get_all_posts() {
@@ -14,6 +14,98 @@ mod posts {
 	async fn get_post_by_id() {
 		let client = DummyJsonClient::default();
 		let response = client.get_post_by_id(1).await;
+		assert!(response.is_ok());
+		println!("{:#?}", response.unwrap());
+	}
+
+	#[tokio::test]
+	async fn search_posts() {
+		let client = DummyJsonClient::default();
+		let response = client.search_posts("love").await;
+		assert!(response.is_ok());
+		println!("{:#?}", response.unwrap());
+	}
+
+	#[tokio::test]
+	async fn limit_and_skip_posts() {
+		let client = DummyJsonClient::default();
+		let response = client.limit_and_skip_posts(10, 10, "title,reactions,userId").await;
+		assert!(response.is_ok());
+		println!("{:#?}", response.unwrap());
+	}
+
+	#[tokio::test]
+	async fn sort_posts() {
+		let client = DummyJsonClient::default();
+		let response = client.sort_posts("title", "desc").await;
+		assert!(response.is_ok());
+		println!("{:#?}", response.unwrap());
+	}
+
+	#[tokio::test]
+	async fn get_all_posts_tags() {
+		let client = DummyJsonClient::default();
+		let response = client.get_all_posts_tags().await;
+		assert!(response.is_ok());
+		println!("{:#?}", response.unwrap());
+	}
+
+	#[tokio::test]
+	async fn get_posts_by_tags() {
+		let client = DummyJsonClient::default();
+		let response = client.get_posts_by_tags("nature").await;
+		assert!(response.is_ok());
+		println!("{:#?}", response.unwrap());
+	}
+
+	#[tokio::test]
+	async fn get_posts_by_user_id() {
+		let client = DummyJsonClient::default();
+		let response = client.get_posts_by_user_id(1).await;
+		assert!(response.is_ok());
+		println!("{:#?}", response.unwrap());
+	}
+
+	#[tokio::test]
+	async fn get_post_comments() {
+		let client = DummyJsonClient::default();
+		let response = client.get_post_comments(1).await;
+		// assert!(response.is_ok());
+		println!("{:#?}", response.unwrap());
+	}
+
+	#[tokio::test]
+	async fn add_post() {
+		let client = DummyJsonClient::default();
+		let post = AddPost {
+			title: Some("test title".to_string()),
+			body: Some("test body".to_string()),
+			user_id: Some(1),
+			tags: Some(vec!["nature".to_string(), "photography".to_string()]),
+			..Default::default()
+		};
+		let response = client.add_post(&post).await;
+		assert!(response.is_ok());
+		println!("{:#?}", response.unwrap());
+	}
+
+	#[tokio::test]
+	async fn update_post() {
+		let client = DummyJsonClient::default();
+		let updated_post = AddPost {
+			title: Some("updated title".to_string()),
+			body: Some("updated body".to_string()),
+			..Default::default()
+		};
+		let response = client.update_post(1, &updated_post).await;
+		assert!(response.is_ok());
+		println!("{:#?}", response.unwrap());
+	}
+
+	#[tokio::test]
+	async fn delete_post() {
+		let client = DummyJsonClient::default();
+		let response = client.delete_post(1).await;
 		assert!(response.is_ok());
 		println!("{:#?}", response.unwrap());
 	}
